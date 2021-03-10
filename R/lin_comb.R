c9adabb1bedf8661855cf06a1cb79f3e637e88a7
josue-rodriguez <josue.rodriguez594@gmail.com>
2021-03-10 09:47:44 -0800

    update data.frame method, docs

--- R/lin_comb.R
+++ R/lin_comb.R
@@ -1,12 +1,14 @@
 #' Perform a linear combination of posterior samples
 #'
-#' @param lin_comb A string specifying a linear combination of variables
+#' @param lin_comb A string specifying a linear combination of variables, or a list of variable names if using \code{contrast}.
 #'
-#' @param obj An object of class \code{BGGM}, \code{bbcor}, or a \code{data.frame} of posterior samples
+#' @param obj An object of class \code{BGGM}, \code{bbcor}, or a \code{data.frame} of posterior samples.
 #'
-#' @param cri_level The level for which a credible interval should be computed
+#' @param cri_level The level for which a credible interval should be computed.
 #'
 #' @param rope Specify a ROPE. Optional.
+#'
+#' @param contrast A contrast matrix specifying which combinations to test.
 #'
 #' @return An object of class \code{lin_comb}
 #'
@@ -33,14 +35,15 @@
 lin_comb <- function(lin_comb,
                      obj,
                      cri_level = 0.90,
-                     rope = NULL) {
+                     rope = NULL,
+                     contrast = NULL) {
 
   if (methods::is(obj, "data.frame")) {
-    out <- lin_comb.data.frame(lin_comb, obj, cri_level, rope)
+    out <- lin_comb.data.frame(lin_comb, obj, cri_level, rope, contrast)
   } else if (methods::is(obj, "BGGM")) {
-    out <- lin_comb.BGGM(lin_comb, obj, cri_level, rope)
+    out <- lin_comb.BGGM(lin_comb, obj, cri_level, rope, contrast)
   } else if (methods::is(obj, "bbcor")) {
-    out <- lin_comb.bbcor(lin_comb, obj, cri_level, rope)
+    out <- lin_comb.bbcor(lin_comb, obj, cri_level, rope, contrast)
   } else {
     stop("Object class not supported. Must be 'BGGM', 'BBcor', or 'data.frame'")
     }
--- R/lin_comb_methods.R
+++ R/lin_comb_methods.R
@@ -39,11 +39,11 @@ lin_comb.BGGM <- function(lin_comb,
       rope_info <- rope_helper(rope,
                                lin_comb[i],
                                quantiles = cri,
-                               post_eval = post_eval,
+                               post_eval = post_eval[, i],
                                ci = ci)
       out_lc <- list(lin_comb = paste0("C", i),
                      rope_overlap = rope_info$rope_overlap,
-                     samples = post_eval,
+                     samples = post_eval[, i],
                      ci = cri,
                      mean_samples = post_mean,
                      sd_samples = post_sd,
@@ -160,11 +160,11 @@ lin_comb.bbcor <- function(lin_comb,
       rope_info <- rope_helper(rope,
                                lin_comb[i],
                                quantiles = cri,
-                               post_eval = post_eval,
+                               post_eval = post_eval[, i],
                                ci = ci)
       out_lc <- list(lin_comb = paste0("C", i),
                      rope_overlap = rope_info$rope_overlap,
-                     samples = post_eval,
+                     samples = post_eval[, i],
                      ci = cri,
                      mean_samples = post_mean,
                      sd_samples = post_sd,
@@ -204,18 +204,22 @@ lin_comb.bbcor <- function(lin_comb,
           # bounds for credible interval
           a <- (1 - ci) / 2
           ci_bounds <- c(a, 1 - a)
-          ci <- quantile(post_eval, ci_bounds)
+          cri <- quantile(post_eval, ci_bounds)
 
           post_mean <- mean(post_eval)
           post_sd <- sd(post_eval)
           prob_greater <- mean(post_eval > 0)
 
-          rope_info <- rope_helper(rope, lin_comb[lc_ind], ci, post_eval)
+          rope_info <- rope_helper(rope,
+                                   lin_comb[lc_ind],
+                                   quantiles = cri,
+                                   post_eval = post_eval,
+                                   ci = ci)
 
           out_lc <- list(lin_comb = lin_comb[lc_ind],
                          rope_overlap = rope_info$rope_overlap,
                          samples = post_eval,
-                         ci = ci,
+                         ci = cri,
                          mean_samples = post_mean,
                          sd_samples = post_sd,
                          prob_greater = prob_greater,
@@ -224,15 +228,19 @@ lin_comb.bbcor <- function(lin_comb,
         }
     }
 
+  if (!is.null(contrast)) {
+    names(lc_list) <- paste0("C", 1:ncol(post_eval))
+  } else {
     names(lc_list) <- paste0("C", seq_along(lin_comb))
+  }
 
-    out$results <- lc_list
-    out$rope <- rope
-    out$ci <- ci
-    out$call <- match.call()
+  out$results <- lc_list
+  out$rope <- rope
+  out$ci <- ci
+  out$call <- match.call()
 
-    class(out) <- "bayeslincom"
-    return(out)
+  class(out) <- "bayeslincom"
+  return(out)
   }
 
 ############################
@@ -247,52 +255,94 @@ lin_comb.data.frame <- function(lin_comb,
   all_vars <- extract_var_names(obj)
 
   out <- lc_list <- list()
-  for (lc_ind in seq_along(lin_comb)) {
-    # extract all correlations in hyp
-    comb <- clean_comb(lin_comb[lc_ind])
-    comb_vars <- find_vars(comb)
 
-    # add backticks for evaluation of hypotheses
-    comb_eval <- comb
-    for (cv in unique(comb_vars)) {
-      comb_eval <- gsub(pattern = cv,
-                        replacement = paste0("`", cv, "`"),
-                        x = comb_eval)
-    }
 
-    # check all parameters in combination are valid
-    miss_pars <- setdiff(comb_vars, all_vars)
-    if (length(miss_pars)) {
-      miss_pars <- paste(miss_pars, collapse = ",")
-      stop(paste("Some variables not found in 'obj' \n", miss_pars))
-    }
+  if (!is.null(contrast)) {
+    post_samps <- obj[, lin_comb]
+    post_samps_mat <- as.matrix(post_samps)
+    post_eval <- t( contrast %*% t(post_samps_mat) )
 
-    # evaluate combinations in post/prior
-    comb_expr <- str2expression(comb_eval)
-    post_eval <- eval(comb_expr, as.data.frame(obj))
-
-    # bounds for credible interval
     a <- (1 - ci) / 2
     cri_bounds <- c(a, 1 - a)
-    cri <- quantile(post_eval, cri_bounds)
+    for (i in 1:ncol(post_eval)) {
+      cri <- quantile(post_eval[, i], cri_bounds)
 
-    post_mean <- mean(post_eval)
-    post_sd <- sd(post_eval)
-    prob_greater <- mean(post_eval > 0)
+      post_mean <- mean(post_eval[, i])
+      post_sd <- sd(post_eval[, i])
+      prob_greater <- mean(post_eval[, i] > 0)
 
-    rope_info <- rope_helper(rope, lin_comb[lc_ind], cri, post_eval)
+      rope_info <- rope_helper(rope,
+                               lin_comb[i],
+                               quantiles = cri,
+                               post_eval = post_eval[, i],
+                               ci = ci)
+      out_lc <- list(lin_comb = paste0("C", i),
+                     rope_overlap = rope_info$rope_overlap,
+                     samples = post_eval[, i],
+                     ci = cri,
+                     mean_samples = post_mean,
+                     sd_samples = post_sd,
+                     prob_greater = prob_greater,
+                     support = rope_info$support)
+      lc_list[[i]] <- out_lc
+    }
+  } else {
+    for (lc_ind in seq_along(lin_comb)) {
+      # extract all correlations in hyp
+      comb <- clean_comb(lin_comb[lc_ind])
+      comb_vars <- find_vars(comb)
 
-    out_lc <- list(lin_comb = lin_comb[lc_ind],
-                   rope_overlap = rope_info$rope_overlap,
-                   samples = post_eval,
-                   cri = cri,
-                   mean_samples = post_mean,
-                   sd_samples = post_sd,
-                   prob_greater = prob_greater,
-                   support = rope_info$support)
-    lc_list[[lc_ind]] <- out_lc
+      # add backticks for evaluation of hypotheses
+      comb_eval <- comb
+      for (cv in unique(comb_vars)) {
+        comb_eval <- gsub(pattern = cv,
+                          replacement = paste0("`", cv, "`"),
+                          x = comb_eval)
+      }
+
+      # check all parameters in combination are valid
+      miss_pars <- setdiff(comb_vars, all_vars)
+      if (length(miss_pars)) {
+        miss_pars <- paste(miss_pars, collapse = ",")
+        stop(paste("Some variables not found in 'obj' \n", miss_pars))
+      }
+
+      # evaluate combinations in post/prior
+      comb_expr <- str2expression(comb_eval)
+      post_eval <- eval(comb_expr, as.data.frame(obj))
+
+      # bounds for credible interval
+      a <- (1 - ci) / 2
+      cri_bounds <- c(a, 1 - a)
+      cri <- quantile(post_eval, cri_bounds)
+
+      post_mean <- mean(post_eval)
+      post_sd <- sd(post_eval)
+      prob_greater <- mean(post_eval > 0)
+
+      rope_info <- rope_helper(rope,
+                               lin_comb[lc_ind],
+                               quantiles = cri,
+                               post_eval = post_eval,
+                               ci = ci)
+
+      out_lc <- list(lin_comb = lin_comb[lc_ind],
+                     rope_overlap = rope_info$rope_overlap,
+                     samples = post_eval,
+                     ci = cri,
+                     mean_samples = post_mean,
+                     sd_samples = post_sd,
+                     prob_greater = prob_greater,
+                     support = rope_info$support)
+      lc_list[[lc_ind]] <- out_lc
+    }
   }
-  names(lc_list) <- paste0("C", seq_along(lin_comb))
+
+  if (!is.null(contrast)) {
+    names(lc_list) <- paste0("C", 1:ncol(post_eval))
+  } else {
+    names(lc_list) <- paste0("C", seq_along(lin_comb))
+  }
 
   out$results <- lc_list
   out$rope <- rope
