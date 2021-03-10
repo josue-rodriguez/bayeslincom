59ab38e11cc316fd2f68cd5d5d7c1ca54ccd79d2
josue-rodriguez <josue.rodriguez594@gmail.com>
2021-03-10 10:21:45 -0800

    update `lin_comb` roxygen doc

--- R/lin_comb.R
+++ R/lin_comb.R
@@ -4,7 +4,7 @@
 #'
 #' @param obj An object of class \code{BGGM}, \code{bbcor}, or a \code{data.frame} of posterior samples.
 #'
-#' @param cri_level The level for which a credible interval should be computed.
+#' @param ci The level for which a credible interval should be computed.
 #'
 #' @param rope Specify a ROPE. Optional.
 #'
@@ -25,7 +25,7 @@
 #' # test
 #' bggm_comb <- lin_comb("a--c + a--d > b--c + b--d",
 #'                        obj = est,
-#'                        cri_level = 0.90,
+#'                        ci = 0.90,
 #'                        rope = c(-0.1, 0.1))
 #'
 #' # print
@@ -34,16 +34,16 @@
 #' @importFrom methods is
 lin_comb <- function(lin_comb,
                      obj,
-                     cri_level = 0.90,
+                     ci = 0.90,
                      rope = NULL,
                      contrast = NULL) {
 
   if (methods::is(obj, "data.frame")) {
-    out <- lin_comb.data.frame(lin_comb, obj, cri_level, rope, contrast)
+    out <- lin_comb.data.frame(lin_comb, obj, ci, rope, contrast)
   } else if (methods::is(obj, "BGGM")) {
-    out <- lin_comb.BGGM(lin_comb, obj, cri_level, rope, contrast)
+    out <- lin_comb.BGGM(lin_comb, obj, ci, rope, contrast)
   } else if (methods::is(obj, "bbcor")) {
-    out <- lin_comb.bbcor(lin_comb, obj, cri_level, rope, contrast)
+    out <- lin_comb.bbcor(lin_comb, obj, ci, rope, contrast)
   } else {
     stop("Object class not supported. Must be 'BGGM', 'BBcor', or 'data.frame'")
     }
